66dbf62c22c5b01fb6dbbb11690e63a4c30886ad
Abdimalik11 <ilkacaze5522@gmail.com>
2023-05-31 18:02:26 -0700

    this is the Answer of paper-scissors

--- challenge.js
+++ challenge.js
@@ -1,2 +1,10 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+function playGame(pl1,pl2) { 
+    if (pl1 === pl2) { 
+    return "tie"; } 
+    else if ( (pl1 === "rock" && pl2 === "scissors") || (pl1 === "paper" && pl2 === "rock") || (pl1 === "scissors" && pl2 === "paper") )
+     { return "pl1 wins"; } 
+    else { return "pl2 wins"; } } 
+    console.log(playGame("rock", "paper"))
+    console.log(playGame("scissors", "scissors"))
